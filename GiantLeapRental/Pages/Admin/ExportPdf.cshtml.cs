21a9b2dabf6d054bb26e7f4fe0771c9c9bcf9768
Adriana Barcenas <abarcenas2020@gmail.com>
2025-04-21 22:14:46 -0500

    idk

---
+++ GiantLeapRental/Pages/Admin/ExportPdf.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System.IO;
+
+namespace GiantLeapRental.Pages.Admin
+{
+    public class ExportPdfModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            var stream = new MemoryStream();
+
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(50);
+                    page.Content().Text("🚀 Hello from GiantLeapRental PDF!").FontSize(24).Bold();
+                });
+            })
+            .GeneratePdf(stream);
+
+            stream.Position = 0;
+            return File(stream.ToArray(), "application/pdf", "test-booking.pdf");
+        }
+    }
+}
